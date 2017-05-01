242f972b5bbfc6489c19b5fa1db03f3c154dd9f3
Chris Matta <chris@confluent.io>
2017-05-01 10:36:04 -0400

    Initial commit

---
+++ src/main/java/Constants.java
@@ -0,0 +1,68 @@
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Timestamp;
+
+/**
+ * Created by chris on 4/28/17.
+ */
+public class Constants {
+    public static final String CREATEDAT = "createdat";
+    public static final String WIKIPAGE = "wikipage";
+    public static final String CHANNEL = "channel";
+    public static final String ISNEW = "isnew";
+    public static final String ISMINOR = "isminor";
+    public static final String ISUNPATROLLED = "isunpatrolled";
+    public static final String ISBOT = "isbot";
+    public static final String DIFFURL = "diffurl";
+    public static final String USERNAME = "username";
+    public static final String BYTECHANGE = "bytechange";
+    public static final String COMMITMESSAGE = "commitmessage";
+
+    public static final Schema SCHEMA = SchemaBuilder.struct()
+        .name("com.github.cjmatta.kafka.connect.transform.wikiedit.WikiEdit")
+        .doc("The parsed Wikipedia Edit")
+        .field(
+            CREATEDAT,
+            Timestamp.builder().doc("The timestamp of the edit").build()
+        )
+        .field(
+            WIKIPAGE,
+            SchemaBuilder.string().doc("The page that was edited").build()
+        )
+        .field(
+            CHANNEL,
+            SchemaBuilder.string().doc("The source channel the edit came through").build()
+        )
+        .field(
+            USERNAME,
+            SchemaBuilder.string().doc("Username of editor").build()
+        )
+        .field(
+            COMMITMESSAGE,
+            SchemaBuilder.string().doc("Commit message for edit.").build()
+        )
+        .field(
+            BYTECHANGE,
+            SchemaBuilder.int32().doc("number of bytes added or removed with this edit.").build()
+        )
+        .field(
+            DIFFURL,
+            SchemaBuilder.string().doc("The URL showing the edit diff.").build()
+        )
+        .field(
+            ISNEW,
+            SchemaBuilder.bool().defaultValue(false).optional().doc("Is this a new page?").build()
+        )
+        .field(
+            ISMINOR,
+            SchemaBuilder.bool().defaultValue(false).optional().doc("Is this edit minor?").build()
+        )
+        .field(
+            ISBOT,
+            SchemaBuilder.bool().defaultValue(false).optional().doc("Is this an edit by a bot?").build()
+        )
+        .field(
+            ISUNPATROLLED,
+            SchemaBuilder.bool().defaultValue(false).optional().doc("Is this edit unpatrolled?").build()
+        ).build();
+}
---
+++ src/main/java/WikiEditTransformation.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright © 2017 Christopher Matta (chris.matta@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.transforms.Transformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class WikiEditTransformation<R extends ConnectRecord<R>> implements Transformation<R> {
+    private static final Logger log = LoggerFactory.getLogger(WikiEditTransformation.class);
+
+    @Override
+    public ConnectRecord apply(ConnectRecord record) {
+        if (null == record.valueSchema() || Schema.Type.STRUCT != record.valueSchema().type()) {
+            log.trace("record.valueSchema() is null or record.valueSchema() is not a struct.");
+            return record;
+        }
+
+        Struct inputRecord = (Struct) record.value();
+        Struct returnStruct = this.parseMessage(inputRecord.getString("message"));
+        returnStruct.put(Constants.CREATEDAT, inputRecord.get("createdat"));
+        returnStruct.put(Constants.CHANNEL, inputRecord.get("channel"));
+
+        return record.newRecord(
+            record.topic(),
+            record.kafkaPartition(),
+            record.keySchema(),
+            record.key(),
+            returnStruct.schema(),
+            returnStruct,
+            record.timestamp()
+        );
+
+    }
+
+    private Struct parseMessage (String message) {
+        String pattern ="\\[\\[(.*)\\]\\]\\s(.*)\\s(.*)\\s\\*\\s(.*)\\s\\*\\s\\(([\\+|\\-].\\d*)\\)\\s?(.*)?$";
+        Pattern wikiPattern = Pattern.compile(pattern);
+        Matcher matcher = wikiPattern.matcher(message);
+        matcher.matches();
+
+        Struct outputRecord = new Struct(Constants.SCHEMA);
+        outputRecord.put(Constants.WIKIPAGE, matcher.group(1));
+        outputRecord.put(Constants.DIFFURL, matcher.group(3));
+        outputRecord.put(Constants.USERNAME, matcher.group(4));
+        outputRecord.put(Constants.BYTECHANGE, Integer.parseInt(matcher.group(5)));
+        outputRecord.put(Constants.COMMITMESSAGE, matcher.group(6));
+
+//        Set Flags
+        outputRecord.put(Constants.ISNEW, matcher.group(2).contains("N"));
+        outputRecord.put(Constants.ISMINOR, matcher.group(2).contains("M"));
+        outputRecord.put(Constants.ISUNPATROLLED, matcher.group(2).contains("!"));
+        outputRecord.put(Constants.ISBOT, matcher.group(2).contains("B"));
+
+        return outputRecord;
+
+    }
+
+    @Override
+    public ConfigDef config() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> map) {
+
+    }
+}
